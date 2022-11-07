fd23b5e3bee52eb131c5833b55157c9cb3662bbd
Paul Tiry <tirypaul@gmail.com>
2022-11-07 17:35:24 +0100

    Initialize project using Create React App

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+function App() {
+  return <Title>Happy Coding!</Title>
+}
+
+export default App
+
+const Title = styled.h1`
+  width: 100vw;
+  height: 60vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
